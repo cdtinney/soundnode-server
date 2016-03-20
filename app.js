6c02f080bafc0cb4f8440cfc8f39ebd209d248e7
cdtinney <colintinney@cmail.carleton.ca>
2016-03-19 21:32:25 -0400

    add NeDB, get/post request handlers for get/share (respectively)

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-
-
 var express = require('express'),
   config = require('./config/config'),
   glob = require('glob')
 
 var app = express();
-
 require('./config/express')(app, config);
+
+var Datastore = require('nedb')
+  , path = require('path')
+var db = {}
+db.users = new Datastore({ filename: __dirname + '/db/' + '/users.db', autoload: true });
+db.playlists = new Datastore({ filename: __dirname + '/db/' + '/playlists.db', autoload: true });
+db.userPlaylist = new Datastore({ filename: __dirname + '/db/' + '/userPlaylist.db', autoload: true });
+db.invites = new Datastore({ filename: __dirname + '/db/' + '/invites.db', autoload: true });
+
+app.set('db', db);
 
 app.listen(config.port, function () {
   console.log('Express server listening on port ' + config.port);
--- app/controllers/home.js
+++ app/controllers/home.js
@@ -5,8 +5,48 @@ module.exports = function (app) {
   app.use('/', router);
 };
 
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
     res.render('index', {
-      title: 'Generator-Express MVC'
+      title: 'SoundNode server'
     });
 });
+
+router.get('/get', function (req, res) {
+
+    var userId = req.query.userId;    
+    if (userId === undefined) {
+        res.sendStatus(400);
+        return;
+    } 
+    
+    var users = findUser(req.app.get('db'), userId);
+    var statusCode = users === null ? 400 : 200;
+    res.sendStatus(statusCode);
+    
+});
+
+router.post('/share', function(req, res) {
+
+    var userId = req.query.userId;
+    var playlistId = req.query.playlistId;    
+    if (userId === undefined || playlistId === undefined) {
+        console.log("1");
+        res.sendStatus(400);
+        return;
+    }
+    
+    var users = findUser(req.app.get('db'), userId);
+    var statusCode = users === null ? 400 : 200;
+    res.sendStatus(statusCode);
+
+});
+
+function findUser(db, userId) {
+        
+    db.users.find({ userId: userId }, function (err, users) {    
+        if (err) return null;
+        console.log("GET user - " + userId + " - Users found: " + users.length);
+        return users;
+    });
+
+}
